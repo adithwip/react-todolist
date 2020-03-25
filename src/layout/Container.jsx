9d87f07093a3610374c50793ba61bf72dbbe1009
Adith Widya Pradipta <adithwidyapradipta@gmail.com>
2020-03-25 10:11:24 +0700

    commit all (see inside for lot of changes)

---
+++ src/layout/Container.jsx
@@ -0,0 +1,93 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import PropTypes from "prop-types";
+
+const Container = ({
+  children,
+  flexDirection,
+  flexWrap,
+  justifyContent,
+  alignItems,
+  alignContent,
+  height
+}) => {
+  const containerStyles = css`
+    display: flex;
+    flex-direction: ${flexDirection};
+    flex-wrap: ${flexWrap};
+    justify-content: ${justifyContent};
+    align-items: ${alignItems};
+    align-content: ${alignContent};
+    height: ${height};
+  `;
+  return (
+    <div className="flex-container" css={containerStyles}>
+      {children}
+    </div>
+  );
+};
+
+Container.defaultProps = {
+  flexDirection: "row",
+  flexWrap: "nowrap",
+  justifyContent: "flex-start",
+  alignItems: "stretch",
+  alignContent: "stretch",
+  height: "auto"
+};
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]),
+  flexDirection: PropTypes.oneOf([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse"
+  ]),
+  flexWrap: PropTypes.oneOf(["nowrap", "wrap", "wrap-reverse"]),
+  justifyContent: PropTypes.oneOf([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+    "start",
+    "end",
+    "left",
+    "right"
+  ]),
+  alignItems: PropTypes.oneOf([
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "baseline",
+    "first baseline",
+    "last baseline",
+    "start",
+    "end",
+    "self-start",
+    "self-end"
+  ]),
+  alignContent: PropTypes.oneOf([
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+    "start",
+    "end",
+    "baseline",
+    "first baseline",
+    "last baseline"
+  ]),
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
+
+export default Container;
